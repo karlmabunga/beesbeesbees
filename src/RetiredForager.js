fdd2128f4368d10b2a2a6df3fdb28192e7e6913f
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-24 14:34:03 -0700

    Completed RetiredForagerBee and all tests pass

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,28 @@
 var RetiredForagerBee = function() {
-};
+  // a food property that is inherited from grub
+  // an eat method that is inherited from grub
+  // a treasureChest property inherited from ForagerBee that is set to an empty array []
+  // call the ForagerBee superclass
+  ForagerBee.call(this);
 
+  // an age property that is set to 40
+  this.age = 40;
+  // a job property that is set to gamble
+  this.job = 'gamble';
+  // a canFly property that is set to false
+  this.canFly = false;
+  // a color property that is set to grey
+  this.color = 'grey';
+};
+// set the prototype
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+// set the constructor
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+// a forage method that returns "I am too old, let me play cards instead"
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+// an always winning gamble method that allows the bee to add a treasure to the treasureChest
+RetiredForagerBee.prototype.gamble = function() {
+  this.treasureChest.push('treasure');
+}
